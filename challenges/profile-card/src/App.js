3c9435fc718744af50d2cc8b5b6ba4bc3c3092c5
Koen Castermans <castermanskoen@msn.com>
2023-08-15 11:51:09 +0200

    First challenge: profile card setup

---
+++ challenges/profile-card/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
