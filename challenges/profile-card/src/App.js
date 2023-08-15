60d593fc0a04458ba82ccad92ab6a8588b9f5b2d
Koen Castermans <castermanskoen@msn.com>
2023-08-15 19:58:09 +0200

    Finish the challenge

--- challenges/profile-card/src/App.js
+++ challenges/profile-card/src/App.js
@@ -11,4 +11,43 @@ function App() {
   );
 }
 
+function Avatar() {
+  return (
+    <img
+      src="https://avatars.githubusercontent.com/u/17332418?v=4"
+      alt="avatar"
+      className="avatar"
+    />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Koen Castermans</h1>
+      <p>
+        Front-end web developer at Wisemen. Has experience across the full stack.
+        In his free time he likes to work out, ride his bike and play video games.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return <ul className="skill-list">
+    <Skill name="HTML/CSS/JS" emoji="💪" color="#e34c26" />
+    <Skill name="Vue 3" emoji="💪" color="green" />
+    <Skill name="Nuxt 3" emoji="💪" color="green" />
+    <Skill name="TailwindCSS" emoji="💪" color="lightblue" />
+    <Skill name="React" emoji="👍" color="lightblue" />
+  </ul>
+}
+
+function Skill({name, emoji, color}) {
+  return <li className="skill" style={{backgroundColor: color}}>
+    <span>{name}</span>
+    <span>{emoji}</span>
+  </li>
+}
+
 export default App;
