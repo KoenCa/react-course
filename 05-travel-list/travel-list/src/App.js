83579281f3d5843ca87fe4006038fb7d31ca77fe
Koen Castermans <castermanskoen@msn.com>
2023-08-21 20:22:09 +0200

    Add static layout

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -1,7 +1,30 @@
 export default function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>
+}
+
+function Form() {
+  return <div className="add-form">
+    <h3>What do you need for your 🥰 trip?</h3>
+  </div>
+}
+function PackingList() {
+  return <div className="list">
+    LIST
+  </div>
+}
+function Stats() {
+  return <footer className="stats">
+    <em>You have X items on your list and you already packed X (X%)</em>
+  </footer>
 }
