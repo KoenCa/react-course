950309f0150396ddc655734cc65906eb6771f287
Koen Castermans <castermanskoen@msn.com>
2023-08-29 20:57:33 +0200

    Travel list: derived state

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -36,7 +36,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onItemPacked={handleItemPacked}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   )
 }
@@ -132,10 +132,28 @@ function Item({
   )
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    )
+
+  const amountOfItems = items.length
+  const amountOfPackedItems = items.filter(item => item.packed).length
+  const percentageOfPackedItems = Math.round(
+    (amountOfPackedItems / amountOfItems) * 100
+  )
+
   return (
     <footer className="stats">
-      <em>You have X items on your list and you already packed X (X%)</em>
+      <em>
+        {percentageOfPackedItems === 100
+          ? 'You got everything! Ready to go ✈️'
+          : `You have ${amountOfItems} items on your list and you already packed
+        ${amountOfPackedItems} (${percentageOfPackedItems}%)`}
+      </em>
     </footer>
   )
 }
