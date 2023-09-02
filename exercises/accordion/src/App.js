965e85fd2829919e7de2b4b0535e83f2a45f1b04
Koen Castermans <castermanskoen@msn.com>
2023-09-02 17:22:05 +0200

    Finish accordion exercise

--- exercises/accordion/src/App.js
+++ exercises/accordion/src/App.js
@@ -1,31 +1,58 @@
-import "./styles.css";
+import { useState } from 'react'
+import './styles.css'
 
 const faqs = [
   {
-    title: "Where are these chairs assembled?",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
   },
   {
-    title: "How long do I have to return my chair?",
-    text:
-      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
   },
   {
-    title: "Do you ship to countries outside the EU?",
-    text:
-      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-  }
-];
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+]
 
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
-  );
+  )
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {faqs.map((faq, index) => (
+        <AccordionItem
+          key={faq.title}
+          number={index}
+          title={faq.title}
+          text={faq.text}
+        />
+      ))}
+    </div>
+  )
+}
+
+function AccordionItem({ number, title, text }) {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function handleToggle() {
+    setIsOpen(isOpen => !isOpen)
+  }
+
+  return (
+    <div className={`item ${isOpen && 'open'}`} onClick={handleToggle}>
+      <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  )
 }
