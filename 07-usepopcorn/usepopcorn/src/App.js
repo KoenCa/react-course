1457f36a741ffc96dffe51f62877d9abd6784acb
Koen Castermans <castermanskoen@msn.com>
2023-09-10 15:44:59 +0200

    Fix some prop drilling using component composition

--- 07-usepopcorn/usepopcorn/src/App.js
+++ 07-usepopcorn/usepopcorn/src/App.js
@@ -55,18 +55,25 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   )
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   )
 }
@@ -102,16 +109,11 @@ function NumResults({ movies }) {
   )
 }
 
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchBox />
-    </main>
-  )
+function Main({ children }) {
+  return <main className="main">{children}</main>
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true)
 
   return (
@@ -119,7 +121,7 @@ function ListBox({ movies }) {
       <button className="btn-toggle" onClick={() => setIsOpen1(open => !open)}>
         {isOpen1 ? '–' : '+'}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   )
 }
@@ -149,7 +151,7 @@ function MovieItem({ movie }) {
   )
 }
 
-function WatchBox() {
+function WatchedBox() {
   const [watched, setWatched] = useState(tempWatchedData)
   const [isOpen2, setIsOpen2] = useState(true)
 
