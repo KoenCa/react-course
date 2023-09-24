0ba1f0944de0c7bea09853390814375066101b8e
Koen Castermans <castermanskoen@msn.com>
2023-09-24 20:13:21 +0200

    Make it a bit nicer

--- challenges/text-expander/src/App.js
+++ challenges/text-expander/src/App.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 function App() {
   return (
     <div>
-      <TextExpander>
+      <TextExpander buttonColor="purple" className='box'>
         Space travel is the ultimate adventure! Imagine soaring past the stars
         and exploring new worlds. It's the stuff of dreams and science fiction,
         but believe it or not, space travel is a real thing. Humans and robots
@@ -18,6 +18,7 @@ function App() {
         expandButtonText="Show text"
         collapseButtonText="Collapse text"
         buttonColor="#ff6622"
+        className='box'
       >
         Space travel requires some seriously amazing technology and
         collaboration between countries, private companies, and international
@@ -26,7 +27,7 @@ function App() {
         foot on the moon or when rovers were sent to roam around on Mars.
       </TextExpander>
 
-      <TextExpander expanded={true} className="box">
+      <TextExpander collapsedNumWords={3} expanded={true} className="box">
         Space missions have given us incredible insights into our universe and
         have inspired future generations to keep reaching for the stars. Space
         travel is a pretty cool thing to think about. Who knows what we'll
