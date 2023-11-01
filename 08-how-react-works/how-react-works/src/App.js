09e27aa0fefcaae8ac2f4e5d182bc6a5e9a0e29f
Koen Castermans <castermanskoen@msn.com>
2023-11-01 15:45:13 +0100

    Add some event handler functions
    As examples of how React state updates are batched and why you should use callback functions when updating state based on current state.

--- 08-how-react-works/how-react-works/src/App.js
+++ 08-how-react-works/how-react-works/src/App.js
@@ -1,33 +1,33 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 const content = [
   {
-    summary: "React is a library for building UIs",
+    summary: 'React is a library for building UIs',
     details:
-      "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    summary: "State management is like giving state a home",
+    summary: 'State management is like giving state a home',
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   },
   {
-    summary: "We can think of props as the component API",
+    summary: 'We can think of props as the component API',
     details:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   },
-];
+]
 
 export default function App() {
   return (
     <div>
       <Tabbed content={content} />
     </div>
-  );
+  )
 }
 
 function Tabbed({ content }) {
-  const [activeTab, setActiveTab] = useState(0);
+  const [activeTab, setActiveTab] = useState(0)
 
   return (
     <div>
@@ -47,26 +47,42 @@ function Tabbed({ content }) {
         <DifferentContent />
       )}
     </div>
-  );
+  )
 }
 
 function Tab({ num, activeTab, onClick }) {
   return (
     <button
-      className={activeTab === num ? "tab active" : "tab"}
+      className={activeTab === num ? 'tab active' : 'tab'}
       onClick={() => onClick(num)}
     >
       Tab {num + 1}
     </button>
-  );
+  )
 }
 
 function TabContent({ item }) {
-  const [showDetails, setShowDetails] = useState(true);
-  const [likes, setLikes] = useState(0);
+  const [showDetails, setShowDetails] = useState(true)
+  const [likes, setLikes] = useState(0)
 
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes(currentLikes => currentLikes + 1)
+  }
+
+  // Batch update examples
+  function handleTripleInc() {
+    setLikes(currentLikes => currentLikes + 1)
+    setLikes(currentLikes => currentLikes + 1)
+    setLikes(currentLikes => currentLikes + 1)
+  }
+
+  function handleUndo() {
+    setShowDetails(true)
+    setLikes(0)
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000)
   }
 
   return (
@@ -75,23 +91,23 @@ function TabContent({ item }) {
       {showDetails && <p>{item.details}</p>}
 
       <div className="tab-actions">
-        <button onClick={() => setShowDetails((h) => !h)}>
-          {showDetails ? "Hide" : "Show"} details
+        <button onClick={() => setShowDetails(h => !h)}>
+          {showDetails ? 'Hide' : 'Show'} details
         </button>
 
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
-  );
+  )
 }
 
 function DifferentContent() {
@@ -99,5 +115,5 @@ function DifferentContent() {
     <div className="tab-content">
       <h4>I'm a DIFFERENT tab, so I reset state 💣💥</h4>
     </div>
-  );
+  )
 }
