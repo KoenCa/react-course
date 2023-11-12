ba1ca93b5eafe08e6c981210b8405d5217a36aa3
Koen Castermans <castermanskoen@msn.com>
2023-11-12 17:19:52 +0100

    Currency converter challenge

---
+++ challenges/currency-converter/src/App.js
@@ -0,0 +1,86 @@
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [amount, setAmount] = useState('')
+  const [fromCurrency, setFromCurrency] = useState('EUR')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [isFetching, setIsFetching] = useState(false)
+  const [result, setResult] = useState('')
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    const abortController = new AbortController()
+
+    async function fetchCurrencyConvertion() {
+      try {
+        setIsFetching(true)
+        setError('')
+
+        const response = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`,
+          { signal: abortController.signal }
+        )
+        const data = await response.json()
+
+        if (!response.ok) throw new Error(data.message)
+
+        setResult(data.rates[toCurrency])
+      } catch (error) {
+        if (error.name === 'AbortError') return
+
+        setError(error.message)
+      } finally {
+        setIsFetching(false)
+      }
+    }
+
+    if (!amount) {
+      setResult('')
+      return
+    }
+
+    fetchCurrencyConvertion()
+
+    return () => abortController.abort()
+  }, [amount, fromCurrency, toCurrency])
+
+  function handleSetFromCurrency(event) {
+    const fromCurrencyInputValue = event.target.value
+    if (fromCurrencyInputValue === toCurrency) return
+
+    setFromCurrency(fromCurrencyInputValue)
+  }
+
+  function handleSetToCurrency(event) {
+    const toCurrencyInputValue = event.target.value
+    if (toCurrencyInputValue === fromCurrency) return
+
+    setToCurrency(toCurrencyInputValue)
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={e => setAmount(e.target.value)}
+      />
+      <select value={fromCurrency} onChange={handleSetFromCurrency}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={toCurrency} onChange={handleSetToCurrency}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      {error ? <p>{error}</p> : <p>{isFetching ? 'Loading...' : result}</p>}
+    </div>
+  )
+}
+
+export default App
