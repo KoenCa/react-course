9dbd1ad9a9516ef773182c3604168c4d874d0b40
Koen Castermans <castermanskoen@msn.com>
2023-11-18 11:27:52 +0100

    usePopcorn: useRef hook to focus search input

--- 07-usepopcorn/usepopcorn/src/components/Search.js
+++ 07-usepopcorn/usepopcorn/src/components/Search.js
@@ -1,10 +1,30 @@
+import { useRef, useEffect } from 'react'
+
 export function Search({ query, setQuery }) {
+  const searchInput = useRef(null)
+
+  useEffect(() => {
+    function keyDownHandler(e) {
+      if (document.activeElement === searchInput.current) return
+      if (e.code !== 'Enter') return
+
+      searchInput.current.focus()
+      setQuery('')
+    }
+
+    document.addEventListener('keydown', keyDownHandler)
+
+    return () => document.removeEventListener('keydown', keyDownHandler)
+  }, [setQuery])
+
   return (
     <input
+      ref={searchInput}
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
-      onChange={e => setQuery(e.target.value)} />
-  );
+      onChange={e => setQuery(e.target.value)}
+    />
+  )
 }
