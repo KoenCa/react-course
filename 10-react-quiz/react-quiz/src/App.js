0c03f25554170ed38b93ef89166558b453a13e58
Koen Castermans <castermanskoen@msn.com>
2023-11-26 12:19:21 +0100

    Setup data fetching with reducer from fake API

--- 10-react-quiz/react-quiz/src/App.js
+++ 10-react-quiz/react-quiz/src/App.js
@@ -1,7 +1,47 @@
+import { useEffect, useReducer } from 'react'
 import Header from './components/Header'
 import Main from './components/Main'
 
+const initialState = {
+  questions: [],
+  status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      }
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`)
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const response = await fetch('http://localhost:8000/questions')
+        const questions = await response.json()
+        dispatch({ type: 'dataReceived', payload: questions })
+      } catch (error) {
+        dispatch({ type: 'dataFailed' })
+      }
+    }
+
+    fetchQuestions()
+  }, [])
+
   return (
     <div className="app">
       <Header />
