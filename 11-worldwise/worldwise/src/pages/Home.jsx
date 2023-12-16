e922ce154b261929068c63d015dd2d7bb7d15c0c
Koen Castermans <castermanskoen@msn.com>
2023-12-16 17:51:46 +0000

    WorldWise: navbar with NavLink's on all pages

--- 11-worldwise/worldwise/src/pages/Home.jsx
+++ 11-worldwise/worldwise/src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom"
+import PageNav from "../components/PageNav"
+
 function Home() {
-    return (
-        <div>
-            WorldWise
-        </div>
-    )
+  return (
+    <div>
+      <PageNav />
+
+      <h1>WorldWise</h1>
+    </div>
+  )
 }
 
 export default Home
--- 11-worldwise/worldwise/src/pages/PageNotFound.jsx
+++ 11-worldwise/worldwise/src/pages/PageNotFound.jsx
@@ -1,6 +1,9 @@
+import PageNav from '../components/PageNav'
+
 function PageNotFound() {
   return (
     <div>
+      <PageNav />
       <h1>Not found</h1>
     </div>
   )
