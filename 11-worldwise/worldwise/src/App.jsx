a0199992b0a1ff5461ab122cd034c6b50f3cd359
Koen Castermans <castermanskoen@msn.com>
2023-12-16 17:41:42 +0000

    WorldWise: simple routing with simple pages

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
-  const x = 23
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import PageNotFound from './pages/PageNotFound'
 
+function App() {
   return (
-    <div>
-      WorldWise
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
