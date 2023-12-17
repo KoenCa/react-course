543b1684aed2357e3281f3e945d41b45e697ef37
Koen Castermans <castermanskoen@msn.com>
2023-12-17 17:55:52 +0000

    WorldWise: explore CSS modules

--- 11-worldwise/worldwise/src/pages/Home.jsx
+++ 11-worldwise/worldwise/src/pages/Home.jsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom"
-import PageNav from "../components/PageNav"
+import PageNav from '../components/PageNav'
 
 function Home() {
   return (
     <div>
       <PageNav />
-
       <h1>WorldWise</h1>
     </div>
   )
