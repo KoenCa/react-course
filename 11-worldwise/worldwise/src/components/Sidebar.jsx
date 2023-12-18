e91c7217b2f96c24a63f9dbff247fb979648da5c
Koen Castermans <castermanskoen@msn.com>
2023-12-18 18:00:30 +0000

    WorldWise: implement nested routes

--- 11-worldwise/worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/worldwise/src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import styles from './Sidebar.module.css'
 import Logo from './Logo'
 import AppNav from './AppNav'
 import AppFooter from './AppFooter'
+import { Outlet } from 'react-router-dom'
 
 function Sidebar() {
   return (
@@ -9,7 +10,8 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
+
       <AppFooter />
     </div>
   )
