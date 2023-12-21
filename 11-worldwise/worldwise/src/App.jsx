cc9d74f9fa2899a7e03e0825afe70521bbee345e
Koen Castermans <castermanskoen@msn.com>
2023-12-21 15:32:00 +0000

    WorldWise: redirect app index with Navigate comp

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
@@ -10,7 +10,6 @@ import CityList from './components/CityList'
 import City from './components/City'
 import CountryList from './components/CountryList'
 import Form from './components/Form'
-
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -44,15 +43,12 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path='cities/:id' element={<City />} />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
