69165fc183352417e8778e3d8cb28ecb0a4ce0a2
Koen Castermans <castermanskoen@msn.com>
2023-12-21 15:03:29 +0000

    WorldWise: lat and lng query params for city

--- 11-worldwise/worldwise/src/components/City.jsx
+++ 11-worldwise/worldwise/src/components/City.jsx
@@ -1,28 +1,38 @@
-import { useParams } from "react-router-dom";
-import styles from "./City.module.css";
+import { useParams, useSearchParams } from 'react-router-dom'
+import styles from './City.module.css'
 
-const formatDate = (date) =>
-  new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    weekday: "long",
-  }).format(new Date(date));
+const formatDate = date =>
+  new Intl.DateTimeFormat('en', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    weekday: 'long',
+  }).format(new Date(date))
 
 function City() {
   const { id } = useParams()
+  const [searchParams, setSearchParams] = useSearchParams()
+  const lat = searchParams.get('lat')
+  const lng = searchParams.get('lng')
 
   // TEMP DATA
   const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+    cityName: 'Lisbon',
+    emoji: '🇵🇹',
+    date: '2027-10-31T15:59:59.138Z',
+    notes: 'My favorite city so far!',
+  }
 
-  const { cityName, emoji, date, notes } = currentCity;
+  const { cityName, emoji, date, notes } = currentCity
 
-  return <h1>CITY {id}</h1>
+  return (
+    <>
+      <h1>CITY {id}</h1>
+      <h1>
+        Position: {lat}, {lng}
+      </h1>
+    </>
+  )
 
   // return (
   //   <div className={styles.city}>
@@ -63,4 +73,4 @@ function City() {
   // );
 }
 
-export default City;
+export default City
