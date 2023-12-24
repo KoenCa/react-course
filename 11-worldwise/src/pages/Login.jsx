74a7fcf2e0dddf0ceb6ce0a2a476c73648c561f0
Koen Castermans <castermanskoen@msn.com>
2023-12-24 21:14:13 +0000

    WorldWise: login/logout challenge

--- 11-worldwise/src/pages/Login.jsx
+++ 11-worldwise/src/pages/Login.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styles from './Login.module.css'
 import PageNav from '../components/PageNav'
+import Button from '../components/Button'
+import { useAuth } from '../contexts/FakeAuthContext'
+import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
+  const navigate = useNavigate()
+  const { isAuthenticated, login } = useAuth()
+
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState('jack@example.com')
   const [password, setPassword] = useState('qwerty')
+
+  useEffect(() => {
+    if (!isAuthenticated) return
+
+    navigate('/app')
+  }, [isAuthenticated, navigate])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    login(email, password)
+  }
 
   return (
     <main className={styles.login}>
@@ -33,7 +50,9 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary" onClick={handleSubmit}>
+            Login
+          </Button>
         </div>
       </form>
     </main>
