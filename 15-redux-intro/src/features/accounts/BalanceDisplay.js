7c18c5f6f87346fdd41e6adb39f7c5fb6dafa36c
Koen Castermans <castermanskoen@msn.com>
2024-02-13 17:51:07 +0100

    Show balance using old way of Redux

--- 15-redux-intro/src/features/accounts/BalanceDisplay.js
+++ 15-redux-intro/src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,21 @@
+import { connect } from 'react-redux'
+
 function formatCurrency(value) {
-  return new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "USD",
-  }).format(value);
+  return new Intl.NumberFormat('en', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(value)
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+  return <div className="balance">{formatCurrency(balance)}</div>
 }
 
-export default BalanceDisplay;
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  }
+}
+
+// Old way of getting some state from Redux instead of the hooks
+export default connect(mapStateToProps)(BalanceDisplay)
