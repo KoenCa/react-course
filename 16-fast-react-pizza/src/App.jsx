af6352bb007f73324a9b46233c5b35f0d050c558
Koen Castermans <castermanskoen@msn.com>
2025-08-12 22:19:33 +0200

    React router setup

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -1,7 +1,35 @@
-import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './ui/Home'
+import Menu from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import Order from './features/order/Order'
+import CreateOrder from './features/order/CreateOrder'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: 'order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />,
+  },
+])
 
 function App() {
-  return <p>Hello World</p>
+  return <RouterProvider router={router} />
 }
 
 export default App
