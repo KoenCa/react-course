7ef49533b12e75548998d2cc8365db34ea290d1b
Koen Castermans <castermanskoen@msn.com>
2025-08-15 19:48:02 +0200

    FRP: responsive styling

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <p className="text-sm font-semibold">Koen</p>;
+  return <p className="hidden md:block text-sm font-semibold">Koen</p>;
 }
 
 export default Username;
