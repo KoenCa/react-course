b3a0b14f94243ec67dd77b75952aeee8a6a22a41
Koen Castermans <castermanskoen@msn.com>
2025-08-25 21:50:39 +0200

    FRP: style the create order form

--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -43,15 +43,15 @@ function CreateOrder() {
   const formErrors = useActionData();
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       {/* Matches the closes route by default so `action` attribute is not needed */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
           <input
-            className="input"
+            className="input grow"
             type="text"
             name="customer"
             required
@@ -59,25 +59,30 @@ function CreateOrder() {
           />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
             <input
-              className="input"
+              className="input w-full"
               type="tel"
               name="phone"
               required
               placeholder="Phone number"
             />
+
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
             <input
-              className="input"
+              className="input w-full"
               type="text"
               name="address"
               required
@@ -86,7 +91,7 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             tabIndex={1}
             className="h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2 focus:outline-none"
@@ -96,7 +101,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
--- 16-fast-react-pizza/src/ui/Button.jsx
+++ 16-fast-react-pizza/src/ui/Button.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 import { tw } from "../utils/helpers";
 
 function Button({ to, type, children, ...buttonProps }) {
-  const baseStyles = tw`inline-block rounded-full bg-yellow-400 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:cursor-pointer hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed`;
+  const baseStyles = tw`inline-block rounded-full bg-yellow-400 text-sm font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:cursor-pointer hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed`;
 
   const styles = {
     primary: tw`${baseStyles} px-4 py-3 md:px-6 md:py-4`,
     small: tw`${baseStyles} px-4 py-2 text-xs md:px-5 md:py-2.5`,
-    secondary: tw`"inline-block rounded-full border-2 border-stone-300 px-4 py-2.5 font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:cursor-pointer hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:ring-2 focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed md:px-6 md:py-3.5`,
+    secondary: tw`"inline-block rounded-full border-2 border-stone-300 px-4 py-2.5 text-sm font-semibold tracking-wide text-stone-400 uppercase transition-colors duration-300 hover:cursor-pointer hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:ring-2 focus:ring-stone-200 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed md:px-6 md:py-3.5`,
   };
 
   if (to)
