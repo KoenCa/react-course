7debeb41e414415ecd28f12a337eff54d766a797
Koen Castermans <castermanskoen@msn.com>
2025-09-29 22:58:04 +0200

    Set up The Wild Oasis project
    With styled components

---
+++ 17-the-wild-oasis/src/App.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import { GlobalStyles } from './styles/GlobalStyles'
+import { Button } from './ui/Button'
+import { Input } from './ui/Input'
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Hello world!')}>Check in</Button>
+        <Button onClick={() => alert('Hello world!')}>Check out</Button>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
+    </>
+  )
+}
