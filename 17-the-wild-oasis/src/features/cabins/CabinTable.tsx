08e635a930a79824c3990b3343ba1063363c76bf
Koen Castermans <castermanskoen@msn.com>
2025-10-27 21:12:10 +0100

    TWO: fitler cabins based on discounts

--- 17-the-wild-oasis/src/features/cabins/CabinTable.tsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.tsx
@@ -3,9 +3,24 @@ import { CabinRow } from './CabinRow'
 import { useListCabins } from './useListCabins'
 import { Table } from '../../ui/Table'
 import { Menus } from '../../ui/Menus'
+import { useSearchParams } from 'react-router-dom'
+import { useMemo } from 'react'
 
 export const CabinTable = () => {
+  const [searchParams] = useSearchParams({ discount: 'all' })
   const { isLoadingCabins, cabins } = useListCabins()
+
+  const filteredCabins = useMemo(() => {
+    if (!cabins) return []
+
+    const filterValue = searchParams.get('discount')
+
+    if (filterValue === 'all') return cabins
+    if (filterValue === 'no-discount')
+      return cabins?.filter(cabin => cabin.discount === 0)
+    if (filterValue === 'with-discount')
+      return cabins?.filter(cabin => cabin.discount && cabin.discount > 0)
+  }, [cabins, searchParams])
 
   if (isLoadingCabins) return <Spinner />
 
@@ -22,7 +37,7 @@ export const CabinTable = () => {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           renderRow={cabin => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
