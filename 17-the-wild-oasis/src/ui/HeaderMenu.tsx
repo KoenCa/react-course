e99ec888e2b33b97415127e24c5c8cca1419549a
Koen Castermans <castermanskoen@msn.com>
2025-11-30 21:21:54 +0100

    TWO: user avatar in header

---
+++ 17-the-wild-oasis/src/ui/HeaderMenu.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { Logout } from "../features/authentication/Logout";
+import { ButtonIcon } from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+export const HeaderMenu = () => {
+  const navigate = useNavigate();
+
+  const handleUserPress = () => {
+    navigate("/account");
+  };
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={handleUserPress}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+};
