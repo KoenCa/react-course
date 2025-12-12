4a3d436598203bda962b0f5778b078f60f5e1acc
Koen Castermans <castermanskoen@msn.com>
2025-12-12 12:27:26 +0000

    TWO: dark mode

---
+++ 17-the-wild-oasis/src/context/DarkModeContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, type ReactNode } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext<{
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+}>({
+  isDarkMode: false,
+  toggleDarkMode: () => {},
+});
+
+export const DarkModeProvider = ({ children }: { children: ReactNode }) => {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((isDark) => !isDark);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export const useDarkMode = () => {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined)
+    throw new Error("DarkModeContext was used outside of DarkModeProvider");
+
+  return context;
+};
