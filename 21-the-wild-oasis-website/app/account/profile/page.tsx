1385cc5c7ea49a376aed39ed3c316681f1490c45
Koen Castermans <castermanskoen@msn.com>
2026-02-23 19:48:48 +0100

    TWOW: nest account routes: profile + reservations

--- 21-the-wild-oasis-website/app/account/profile/page.tsx
+++ 21-the-wild-oasis-website/app/account/profile/page.tsx
@@ -1,12 +1,11 @@
-import SelectCountry from "@/app/_components/SelectCountry";
+import { SelectCountry } from "@/components/SelectCountry";
 
-export default function Page() {
-  // CHANGE
-  const countryFlag = "pt.jpg";
+export default function Profile() {
+  const countryflag = "pt.jpg";
   const nationality = "portugal";
 
   return (
-    <div>
+    <main>
       <h2 className="font-semibold text-2xl text-accent-400 mb-4">
         Update your guest profile
       </h2>
@@ -18,16 +17,18 @@ export default function Page() {
 
       <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
         <div className="space-y-2">
-          <label>Full name</label>
+          <label htmlFor="fullName">Full name</label>
           <input
+            id="fullName"
             disabled
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled-text-gray-400"
           />
         </div>
 
         <div className="space-y-2">
-          <label>Email address</label>
+          <label htmlFor="email">Email address</label>
           <input
+            id="email"
             disabled
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
           />
@@ -37,15 +38,15 @@ export default function Page() {
           <div className="flex items-center justify-between">
             <label htmlFor="nationality">Where are you from?</label>
             <img
-              src={countryFlag}
-              alt="Country flag"
+              src={countryflag}
+              alt="country flag"
               className="h-5 rounded-sm"
             />
           </div>
 
           <SelectCountry
-            name="nationality"
             id="nationality"
+            name="nationality"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             defaultCountry={nationality}
           />
@@ -60,11 +61,11 @@ export default function Page() {
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300 hover:cursor-pointer">
             Update profile
           </button>
         </div>
       </form>
-    </div>
+    </main>
   );
 }
--- 21-the-wild-oasis-website/app/account/reservations/page.tsx
+++ 21-the-wild-oasis-website/app/account/reservations/page.tsx
@@ -1,11 +1,11 @@
-import ReservationCard from "./ReservationCard";
+import Link from "next/link";
+import { ReservationCard } from "@/components/ReservationCard";
 
-export default function Page() {
-  // CHANGE
+export default function Reservations() {
   const bookings = [];
 
   return (
-    <div>
+    <main>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
         Your reservations
       </h2>
@@ -13,17 +13,17 @@ export default function Page() {
       {bookings.length === 0 ? (
         <p className="text-lg">
           You have no reservations yet. Check out our{" "}
-          <a className="underline text-accent-500" href="/cabins">
-            luxury cabins &rarr;
-          </a>
+          <Link href="/cabins" className="underline text-accent-500">
+            Luxury cabins &rarr;
+          </Link>
         </p>
       ) : (
         <ul className="space-y-6">
           {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
+            <ReservationCard key={booking.id} booking={booking} />
           ))}
         </ul>
       )}
-    </div>
+    </main>
   );
 }
