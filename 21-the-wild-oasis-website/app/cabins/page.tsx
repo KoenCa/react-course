9af46d1a062239627a866b22711316d64cff07f5
Koen Castermans <castermanskoen@msn.com>
2026-03-08 10:31:55 +0100

    TWOW: move cabins fetching into own component
    So that this async component can be wrapped into a Suspense component and display the loading spinner below the fixed/static text content which leads to a better UX

--- 21-the-wild-oasis-website/app/cabins/page.tsx
+++ 21-the-wild-oasis-website/app/cabins/page.tsx
@@ -1,14 +1,13 @@
+import { CabinList } from "@/components/CabinList";
+import { Spinner } from "@/components/Spinner";
 import { Metadata } from "next";
-import { CabinCard } from "@/components/CabinCard";
-import { getCabins } from "@/lib/data-service";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Cabins",
 };
 
-export default async function Cabins() {
-  const cabins = await getCabins()
-
+export default function Cabins() {
   return (
     <main>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Cabins</h1>
@@ -16,18 +15,14 @@ export default async function Cabins() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolom ites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard key={cabin.id} cabin={cabin} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </main>
   );
 }
