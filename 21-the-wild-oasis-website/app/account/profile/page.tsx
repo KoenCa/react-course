a342fd78dc795b47b1403339dc574d8ee5c1605a
Koen Castermans <castermanskoen@msn.com>
2026-05-03 12:43:48 +0200

    TWOW: use server component in client component
    By importing it in a server component and passing it to the client component as a prop

--- 21-the-wild-oasis-website/app/account/profile/page.tsx
+++ 21-the-wild-oasis-website/app/account/profile/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { UpdateProfileForm } from "@/components/UpdateProfileForm";
 import { SelectCountry } from "@/components/SelectCountry";
 
 export const metadata: Metadata = {
@@ -6,7 +7,6 @@ export const metadata: Metadata = {
 };
 
 export default function Profile() {
-  const countryflag = "pt.jpg";
   const nationality = "portugal";
 
   return (
@@ -20,57 +20,14 @@ export default function Profile() {
         faster and smoother. See you soon!
       </p>
 
-      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
-        <div className="space-y-2">
-          <label htmlFor="fullName">Full name</label>
-          <input
-            id="fullName"
-            disabled
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled-text-gray-400"
-          />
-        </div>
-
-        <div className="space-y-2">
-          <label htmlFor="email">Email address</label>
-          <input
-            id="email"
-            disabled
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-          />
-        </div>
-
-        <div className="space-y-2">
-          <div className="flex items-center justify-between">
-            <label htmlFor="nationality">Where are you from?</label>
-            <img
-              src={countryflag}
-              alt="country flag"
-              className="h-5 rounded-sm"
-            />
-          </div>
-
-          <SelectCountry
-            id="nationality"
-            name="nationality"
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-            defaultCountry={nationality}
-          />
-        </div>
-
-        <div className="space-y-2">
-          <label htmlFor="nationalID">National ID number</label>
-          <input
-            name="nationalID"
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
-          />
-        </div>
-
-        <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300 hover:cursor-pointer">
-            Update profile
-          </button>
-        </div>
-      </form>
+      <UpdateProfileForm>
+        <SelectCountry
+          id="nationality"
+          name="nationality"
+          className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+          defaultCountry={nationality}
+        />
+      </UpdateProfileForm>
     </main>
   );
 }
